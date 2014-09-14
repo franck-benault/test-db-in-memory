4209ffcca9b9738df000bd25e0cddca4e3f4e225
franck-benault <franck-benault@orange.fr>
2014-09-14 14:58:02 +0200

    mod03 jdbc advanced add Derby part

---
+++ mod03-jdbc-advanced/src/main/java/net/franckbenault/jdbc/derby/DerbyServer.java
@@ -0,0 +1,65 @@
+package net.franckbenault.jdbc.derby;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import net.franckbenault.jdbc.DBServerInterface;
+
+public class DerbyServer implements DBServerInterface {
+
+	private Connection connection;
+	
+	public void start() throws ClassNotFoundException, SQLException {
+	
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		connection = DriverManager.
+		    getConnection("jdbc:derby:memory:testDB;create=true");
+
+	}
+	
+	public int executeQueryUpdate(String query) throws SQLException {
+        PreparedStatement ps2 = connection.prepareStatement(
+        		query );
+        return ps2.executeUpdate();
+	}
+	
+	public int countTables() throws SQLException {
+	
+        PreparedStatement ps2 = connection.prepareStatement(
+        		"select s.schemaname || '.' || t.tablename from sys.systables t, sys.sysschemas s "
+        		+ "where t.schemaid = s.schemaid and t.tabletype = 'T' "
+        		+ "order by s.schemaname, t.tablename" );
+        ResultSet rs = ps2.executeQuery();
+
+        int size = 0;
+        try {
+            while(rs.next()){
+                size++;
+            }
+        }
+        catch(Exception ex) {
+            System.out.println("------------------Tablerize.getRowCount-----------------");
+            System.out.println("Cannot get resultSet row count: " + ex);
+            System.out.println("--------------------------------------------------------");
+        }
+        return size;
+
+	}
+	
+	public void stop() throws SQLException {
+		connection.close();
+	}
+
+	public int countConstraints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getDBVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ mod03-jdbc-advanced/src/test/java/net/franckbenault/jdbc/derby/DerbyServerTest.java
@@ -0,0 +1,55 @@
+package net.franckbenault.jdbc.derby;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import net.franckbenault.jdbc.DBServerInterface;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DerbyServerTest {
+	
+	private static DBServerInterface server;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		server = new DerbyServer();
+		server.start();
+	
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		server.stop();
+	}
+
+	@Test
+	public void testCountTables() throws SQLException {
+		assertEquals(server.countTables(), 0);
+	}
+	
+	@Test
+	public void testCountTablesWithRequest() throws SQLException {
+		assertEquals(server.countTables(), 0);
+		
+		String request = "CREATE TABLE HOTELAVAILABILITY"+
+			    "(HOTEL_ID INT NOT NULL, BOOKING_DATE DATE NOT NULL,"+
+				"ROOMS_TAKEN INT DEFAULT 0, PRIMARY KEY (HOTEL_ID, BOOKING_DATE))";
+
+		server.executeQueryUpdate(request);
+		assertEquals(server.countTables(), 1);
+		
+		String request2 = "DROP TABLE HOTELAVAILABILITY";
+		
+		server.executeQueryUpdate(request2);
+		assertEquals(server.countTables(), 0);
+
+
+	}
+	
+
+}
